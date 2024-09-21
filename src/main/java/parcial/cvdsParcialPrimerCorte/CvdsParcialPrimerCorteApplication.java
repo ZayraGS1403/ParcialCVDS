9db427206683980689c51f782052482645009537
unknown <zayra.gutierrez-s@labinfo.is.escuelaing.edu.co>
2024-09-21 10:17:45 -0500

    subir repositorio de spring inizlr

---
+++ src/main/java/parcial/cvdsParcialPrimerCorte/CvdsParcialPrimerCorteApplication.java
@@ -0,0 +1,13 @@
+package parcial.cvdsParcialPrimerCorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvdsParcialPrimerCorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvdsParcialPrimerCorteApplication.class, args);
+	}
+
+}
